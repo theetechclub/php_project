f859415db2cf2f5f10acb593f428513bfb2b6f9e
Abhishek Verma <www.abhiverma23@gmail.com>
2016-04-15 11:27:09 +0530

    Add turn On and Off Feature

--- scripts/check.php
+++ scripts/check.php
@@ -1,16 +1,17 @@
 <?php
 /* TODO "DirnameFromRoot" is to be replaced with 
  	"/sys/..." string because i forget it's exact location
-	and also see the line 12 or near and replace "DirStartName"
+	and also see the line 7 or near and replace "DirStartName"
 	with the name of folder with gpio number... :) */
-define("LOCATION", "DirnameFromRoot");
+define("LOCATION", "DirnameFromRoot");//Include forward slash in last
+define("DIRSTARTNAME", "DirStartName");
 function check(int gpio){
 	if(!file_exists(LOCATION.$gpio))
 	{
 		system("echo $gpio > ".LOCATION."export");
 		sleep(1);
 		if(!file_exists(LOCATION.$gpio))return 0;//File not creted even
-		system("echo out > ".LOCATION.""."direction");
+		system("echo out > ".LOCATION.DIRSTARTNAME.$gpio."/direction");
 		return 2;//2 means file created
 	}
 	return 1;//File already exist
--- switch.php
+++ switch.php
@@ -4,8 +4,16 @@ include("/scripts/check.php");
 $gpio = $_GET['gpio'];
 $toDo = $_GET['toDo'];
 
-if(check(gpio)){
-	// TODO code to turn ON or OFF led
-}
 
+$string = "echo $toDo > ".LOCATION.DIRSTARTNAME."$gpio/value";
+
+if(check($gpio)){
+	// TODO code to turn ON or OFF led
+	system($string);
+	echo json_encode(array('result' => 'success' ));
+}
+else
+{
+	echo json_encode(array('result' => 'fail' ));
+}
 ?>
